b0ce3d5fc9fb4f5519cf12dabed415e1b8fb4e5a
ChenGang <chengang@zzltop.com>
2024-11-01 18:17:36 +0800

    feat：添加netlify部署配置

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,11 +1,11 @@
-const serverless = require('serverless-http');
- const generateConfig = require('../../generateConfig')
+const serverless = require('serverless-http')
+const generateConfig = require('../../generateConfig')
 async function start() {
   await generateConfig()
   return require('../../server').serveNcmApi({
     checkVersion: true,
   })
 }
-const app = start();
+const app = start()
 
- module.exports.handler = serverless(app);
+module.exports.handler = serverless(app)
