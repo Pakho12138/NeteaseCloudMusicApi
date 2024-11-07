64c4a78461947bdb2a6369719b923655e46b8ea7
ChenGang <chengang@zzltop.com>
2024-11-07 11:53:32 +0800

    log

--- server.js
+++ server.js
@@ -308,6 +308,7 @@ async function serveNcmApi(options) {
 
   /** @type {import('express').Express & ExpressExtension} */
   const appExt = app
+  console.log('--------------------app--------------------', app)
   // appExt.server = app.listen(port, host, () => {
   //   console.log(`server running @ http://${host ? host : 'localhost'}:${port}`)
   // })
