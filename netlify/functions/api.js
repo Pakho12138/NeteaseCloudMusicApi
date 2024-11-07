7825aad9ae7228b74ad86ba40996ee1fa98b7f0a
ChenGang <chengang@zzltop.com>
2024-11-07 15:34:24 +0800

    feat：兼容netlify

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,7 +2,7 @@ exports.handler = async function (event, context) {
   const serverless = require('serverless-http')
   const generateConfig = require('../../generateConfig')
   await generateConfig()
-  const app = await require('../../server').serveNcmApi({
+  const app = await require('../../server.netlify').serveNcmApi({
     checkVersion: true,
   })
   const server = serverless(app)
