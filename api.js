f378882a75c0382a9b378c7f30b6c8a9b3bf0ada
ChenGang <chengang@zzltop.com>
2025-07-18 17:05:20 +0800

    feat：cloudflare部署

--- api.js
+++ api.js
@@ -1,14 +1,22 @@
-exports.handler = async function (event, context) {
-  const serverless = require('serverless-http')
-  const generateConfig = require('./generateConfig')
-  await generateConfig()
-  const app = await require('./server.netlify').serveNcmApi({
-    checkVersion: true,
-  })
-  const server = serverless(app)
-  const result = await server(event, context)
-  // console.log('---------------------result-------------------', result)
-  // console.log('---------------------event-------------------', event)
-  // console.log('---------------------context-------------------', context)
-  return result
+export default {
+  async fetch(request, env, ctx) {
+    const serverless = require('serverless-http')
+    const generateConfig = require('./generateConfig')
+    await generateConfig()
+    const app = await require('./server.netlify').serveNcmApi({
+      checkVersion: true,
+    })
+    const server = serverless(app)
+    const result = await server(env, ctx)
+    console.log('---------------------result-------------------', result)
+    console.log('---------------------event-------------------', env)
+    console.log('---------------------context-------------------', context)
+    return result
+  },
 }
+
+// export default {
+//   async fetch(request, env, ctx) {
+//     return new Response('Hello World!');
+//   },
+// };
