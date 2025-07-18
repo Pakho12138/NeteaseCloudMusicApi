7f2686ffeccbf5ab1a7769cee4101e032fb49320
ChenGang <chengang@zzltop.com>
2025-07-18 17:35:43 +0800

    test

--- functions/api.js
+++ functions/api.js
@@ -1,22 +1,22 @@
-// export default {
-//   async fetch(request, env, ctx) {
-//     const serverless = require('serverless-http')
-//     const generateConfig = require('../../generateConfig')
-//     await generateConfig()
-//     const app = await require('../../server.netlify').serveNcmApi({
-//       checkVersion: true,
-//     })
-//     const server = serverless(app)
-//     const result = await server(env, ctx)
-//     console.log('---------------------result-------------------', result)
-//     console.log('---------------------event-------------------', env)
-//     console.log('---------------------context-------------------', context)
-//     return result
-//   },
-// }
-
 export default {
   async fetch(request, env, ctx) {
-    return new Response('Hello World!')
+    const serverless = require('serverless-http')
+    const generateConfig = require('../../generateConfig')
+    await generateConfig()
+    const app = await require('../../server.netlify').serveNcmApi({
+      checkVersion: true,
+    })
+    const server = serverless(app)
+    const result = await server(env, ctx)
+    console.log('---------------------result-------------------', result)
+    console.log('---------------------event-------------------', env)
+    console.log('---------------------context-------------------', context)
+    return result
   },
 }
+
+// export default {
+//   async fetch(request, env, ctx) {
+//     return new Response('Hello World!');
+//   },
+// };
